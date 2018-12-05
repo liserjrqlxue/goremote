f8436801981a8461e6613d27cf5ae30b764464f3
Wang Yaoshen <wangyaoshen@bgi.com>
2018-12-05 18:49:58 +0800

    add upload download to package sftp

---
+++ sftp/sftp.go
@@ -0,0 +1,54 @@
+package sftp
+
+import (
+	"fmt"
+	"github.com/liserjrqlxue/simple-util"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+	"io"
+	"os"
+)
+
+func upload(conn *ssh.Client, src, dest string) {
+	// create new SFTP client
+	client, err := sftp.NewClient(conn)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(client)
+
+	srcFile, err := os.Open(src)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(srcFile)
+
+	dstFile, err := client.Create(dest)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(dstFile)
+
+	// copy source file to destination file
+	bytes, err := io.Copy(dstFile, srcFile)
+	simple_util.CheckErr(err)
+	fmt.Printf("%d bytes upload\n", bytes)
+}
+
+func download(conn *ssh.Client, src, dest string) {
+	// create new SFTP client
+	client, err := sftp.NewClient(conn)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(client)
+
+	srcFile, err := client.Open(src)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(srcFile)
+
+	dstFile, err := os.Create(dest)
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(dstFile)
+
+	// copy source file to destination file
+	bytes, err := io.Copy(dstFile, srcFile)
+	simple_util.CheckErr(err)
+	fmt.Printf("%d bytes download\n", bytes)
+
+	// flush in-memory copy
+	err = dstFile.Sync()
+	simple_util.CheckErr(err)
+}
